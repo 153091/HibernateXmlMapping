29864aa979951fb4eca29022f5a158030239273d
18741847 <AOKulakov@sberbank.ru>
2020-11-10 09:39:54 +0300

    Initial commit

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,78 @@
+package entity;
+
+import java.sql.Date;
+import java.util.Objects;
+import java.util.Set;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private Address address; // один к одному
+    private Set<Project> projects; // один емплой может иметь много проектов, один проект может иметь много емплойев
+
+    public Employee() {}
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", address=" + address +
+                '}';
+    }
+
+}
